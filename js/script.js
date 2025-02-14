17377b0b92638c3070fc55aa47234c548aff615c
gun-kajiwara <gunkajiwara@gmail.com>
2025-02-14 18:29:47 +0900

    fix: TOP-menu fadein animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-(function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open'; // class
+(function ($) {
+  var $nav = $("#navArea");
+  var $btn = $(".toggle_btn");
+  var $mask = $("#mask");
+  var open = "open"; // class
   // menu open close
-  $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-      $nav.addClass( open );
+  $btn.on("click", function () {
+    if (!$nav.hasClass(open)) {
+      $nav.addClass(open);
     } else {
-      $nav.removeClass( open );
+      $nav.removeClass(open);
     }
   });
   // mask close
-  $mask.on('click', function() {
-    $nav.removeClass( open );
+  $mask.on("click", function () {
+    $nav.removeClass(open);
   });
-} )(jQuery);
+})(jQuery);
+
+(function ($) {
+  // scroll fade-in animation
+  $(window).scroll(function () {
+    $(".js-fadein").each(function () {
+      var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+
+      if (scroll > elemPos - windowHeight + 250) {
+        $(this).addClass("js-scrollin");
+      }
+    });
+  });
+})(jQuery);
